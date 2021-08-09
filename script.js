e8a3cd708069595c3f057a398421cf71b2885259
awri.l <notawril@gmail.com>
2021-08-09 05:39:54 -0700

    INIT (DESIGN PROTOTYPE IS DONE)

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// fetch("https://api.openweathermap.org/data/2.5/onecall?lat=50.619900&lon=26.251617&exclude=hourly&appid=e6d804f5e9320c6522624536e3ac2b78").then(res=>res.json())
+// .then(data=>console.log(data))
+
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
