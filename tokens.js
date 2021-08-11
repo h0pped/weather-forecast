da961b211ba833d0e38208a774c61b31c746a195
awri.l <notawril@gmail.com>
2021-08-11 12:06:21 -0700

    removed gitignore for netlify work

---
+++ tokens.js
@@ -0,0 +1,5 @@
+const config = {
+    WEATHER_TOKEN: "e6d804f5e9320c6522624536e3ac2b78",
+    PLACES_TOKEN: "pk.eyJ1IjoiYXdyaWwiLCJhIjoiY2tyOTRnaHkwMGl2YjJwcDhoYmhkdWNxaiJ9.XlGjFeM6ofVunfyStLiFmQ",
+    MAP_TOKEN: "pk.eyJ1IjoiYXdyaWwiLCJhIjoiY2tyOTRnaHkwMGl2YjJwcDhoYmhkdWNxaiJ9.XlGjFeM6ofVunfyStLiFmQ"
+}
